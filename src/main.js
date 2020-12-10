486c0be6eb771387ed914fe3da59d7a909b57ca9
ruruisu <zaddozgl@gmail.com>
2020-12-10 02:15:46 -0600

    coded esPrimo function

--- src/main.js
+++ src/main.js
@@ -25,12 +25,24 @@ export default class  App {
     }
 
     //
-    esPrimo(){
-        
+    esPrimo(num){
+        let isPrime;
+        let i = 2;
+        do {
+            console.log(i);
+        if (num % i === 0) {
+            return false;
+        }else if(num % num == 0){
+            return true;
+        }
+        i++;
+        } while (i < num);
+
     }
 
     //
     obtenerMultiplos(){
+
 
     }
 
@@ -46,6 +58,7 @@ let app = new App();
 //Console testing
 console.log(app.sumatoriaSerieUno(7));
 console.log(app.sumatoriaSerieDos(6));
+console.log(app.esPrimo(9));
 
 
 
